e32c2fa0d81c3d9468bbec74bbd9956bb3294b69
Bryan <BryanTorres09@users.noreply.github.com>
2025-11-17 13:44:01 -0400

    added ssl to pg client

--- Backend/index.js
+++ Backend/index.js
@@ -38,13 +38,14 @@ const upload = multer({storage});
 app.use(express.static('public'));
 
 //PostgreSQL Client Setup
-const db = new pg.Client ({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-})
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false },
+});
 
 db.connect()
 
