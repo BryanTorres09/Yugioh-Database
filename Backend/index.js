0dc5998b726963ef2895a79b187d379d637dc131
Bryan <BryanTorres09@users.noreply.github.com>
2025-11-17 14:44:17 -0400

    fix for backend on render

--- Backend/index.js
+++ Backend/index.js
@@ -38,12 +38,13 @@ const upload = multer({storage});
 app.use(express.static('public'));
 
 //PostgreSQL Client Setup
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+const db = new pg.Pool({
+  //   user: process.env.PG_USER,
+  //   host: process.env.PG_HOST,
+  //   database: process.env.PG_DATABASE,
+  //   password: process.env.PG_PASSWORD,
+  //   port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
